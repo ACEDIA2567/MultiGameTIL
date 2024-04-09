dc26d9b7bd359821586123584eabb2f391c2fa5c
ACEDIA2567 <101154683+ACEDIA2567@users.noreply.github.com>
2024-04-09 17:21:55 +0900

    Add files via upload

---
+++ Script/Script/Networking/Shared/UserData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class UserData
+{
+    public string userName;   // 이름
+    public string userAuthId; // 아이디
+}
